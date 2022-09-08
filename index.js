b8750000d74acff8972e5d51cf18240bfa6de53b
Akinwumi sehinde <akinwumisehinde@gmail.com>
2022-09-08 13:38:42 +0100

    First commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import  express  from "express";
 import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    console.log('[TEST]!');
 
-    res.send('Hello from hompage.');
+app.use('/', usersRoutes)
 
-});
+app.get('/', (req, res) => res.send('Hello from hompage.'));
 
 app.listen(PORT, () => console.log(`server running on port: http://localhost:${PORT}`))
